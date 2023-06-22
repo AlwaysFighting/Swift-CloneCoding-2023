ac045074bf27fedb25dfe763edfe1f50ecd30835
AlwaysFighting <zosejiri@gmail.com>
2023-06-22 22:56:55 +0900

    init commit

---
+++ CreatedFamousSaying/CreatedFamousSaying/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  CreatedFamousSaying
+//
+//  Created by 목정아 on 2023/06/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
