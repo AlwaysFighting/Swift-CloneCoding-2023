91abd832445ff5f95d566774970f94029dfd07bd
AlwaysFighting <zosejiri@gmail.com>
2023-06-26 16:47:46 +0900

    Feat: gitignroe file 생성

---
+++ Wheather/Wheather/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Wheather
+//
+//  Created by 목정아 on 2023/06/26.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ Wheather/Wheather/WheatherInformation.swift
@@ -0,0 +1,8 @@
+//
+//  WheatherInformation.swift
+//  Wheather
+//
+//  Created by 목정아 on 2023/06/26.
+//
+
+import Foundation
