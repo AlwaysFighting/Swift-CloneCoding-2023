fc6b54605cb5c3a6eb207d77d755c438e323f15b
AlwaysFighting <zosejiri@gmail.com>
2023-06-26 16:49:01 +0900

    Feat: gitinore 수정

--- Wheather/Wheather/ViewController.swift
+++ Wheather/Wheather/ViewController.swift
@@ -1,18 +1,30 @@
-//
-//  ViewController.swift
-//  Wheather
-//
-//  Created by 목정아 on 2023/06/26.
-//
-
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    @IBOutlet weak var cityNameTextField: UITextField!
+    @IBOutlet weak var cityNameLabel: UILabel!
+    @IBOutlet weak var wheatherDescription: UILabel!
+    @IBOutlet weak var tempLabel: UILabel!
+    @IBOutlet weak var maxTempLabel: UILabel!
+    @IBOutlet weak var minTempLabel: UILabel!
+    
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
     }
+    
+    
+    @IBAction func tapFetchWheatherButton(_ sender: UIButton) {
+        
+    }
+    
+    func getCurrentWeather(cityName: String) {
+        let url = URL(string: "https://api.openweathermap.org/data/2.5/weather?q=\(cityName)&appid=")
+    }
+    
 
 
 }
--- Wheather/Wheather/WheatherInformation.swift
+++ Wheather/Wheather/WheatherInformation.swift
@@ -1,8 +1,34 @@
-//
-//  WheatherInformation.swift
-//  Wheather
-//
-//  Created by 목정아 on 2023/06/26.
-//
-
 import Foundation
+
+struct WheatherInformation: Codable {
+    let weather: [Weather]
+    let temp: Temp
+    let name: String
+    
+    enum CodingKeys: String, CodingKey {
+        case weather
+        case temp = "main"
+        case name
+    }
+}
+
+struct Weather: Codable {
+    let id: Int
+    let main: String
+    let description: String
+    let icon: String
+}
+
+struct Temp: Codable {
+    let temp: Double
+    let feelsLike: Double
+    let minTemp: Double
+    let maxTemp: Double
+    
+    enum CodingKeys: String, CodingKey {
+        case temp
+        case feelsLike = "feels_like"
+        case minTemp = "temp_min"
+        case maxTemp = "temp_max"
+    }
+}
