4a58ba156d425ac75e4c67df06ffc5f6a5a86251
AlwaysFighting <zosejiri@gmail.com>
2023-06-30 15:25:42 +0900

    Feat: LocalNotification APP 완료

---
+++ DrinkWaterNotification/DrinkWaterNotification/AddAlertViewController.swift
@@ -0,0 +1,26 @@
+import UIKit
+
+class AddAlertViewController: UIViewController {
+
+    var pickedDate: ((_ date: Date) -> Void)?
+    
+    
+    @IBOutlet weak var datePicker: UIDatePicker!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+    
+    @IBAction func dismissButtonTapped(_ sender: UIBarButtonItem) {
+        self.dismiss(animated: true)
+    }
+    
+    @IBAction func saveButtonTapped(_ sender: UIBarButtonItem) {
+        pickedDate?(datePicker.date)
+        self.dismiss(animated: true, completion: nil)
+    }
+    
+   
+
+}
---
+++ DrinkWaterNotification/DrinkWaterNotification/AlertListCell.swift
@@ -0,0 +1,31 @@
+import UIKit
+import UserNotifications
+
+class AlertListCell: UITableViewCell {
+    
+    var userNotificationCenter = UNUserNotificationCenter.current()
+    
+    @IBOutlet weak var meridiemLabel: UILabel!
+    @IBOutlet weak var timeLabel: UILabel!
+    @IBOutlet weak var alertSwitch: UISwitch!
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+    }
+    
+    @IBAction func alertSwitch(_ sender: UISwitch) {
+        guard let data = UserDefaults.standard.value(forKey: "alerts") as? Data,
+              var alerts = try? PropertyListDecoder().decode([Alert].self, from: data) else { return }
+        alerts[sender.tag].isOn = sender.isOn
+        UserDefaults.standard.set(try? PropertyListEncoder().encode(alerts), forKey: "alerts")
+        
+        if sender.isOn {
+            userNotificationCenter.addNotificationRequest(by: alerts[sender.tag])
+        } else {
+            userNotificationCenter.removePendingNotificationRequests(withIdentifiers: [alerts[sender.tag].id])
+        }
+    }
+    
+    
+    
+}
