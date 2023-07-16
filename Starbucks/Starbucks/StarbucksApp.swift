0aa7757a646d82bce234d1d4a8d8b19a55b8760e
AlwaysFighting <zosejiri@gmail.com>
2023-07-17 02:49:59 +0900

    Feat: SwiftUI - Starbucks Cafe App Start

---
+++ Starbucks/Starbucks/StarbucksApp.swift
@@ -0,0 +1,17 @@
+//
+//  StarbucksApp.swift
+//  Starbucks
+//
+//  Created by 목정아 on 2023/07/17.
+//
+
+import SwiftUI
+
+@main
+struct StarbucksApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
